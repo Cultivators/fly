bd37ffe538a73be39da56a1dae95069c052f53dc
Cultivators <wuxinjie13450@gmail.com>
2019-05-11 20:38:43 +0800

    调通HelloController接口

--- src/main/java/com/mpp/fly/HelloWorldController.java
+++ src/main/java/com/mpp/fly/HelloWorldController.java
@@ -1,8 +1,31 @@
 package com.mpp.fly;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/hello")
 public class HelloWorldController {
-    //Hello world
+
+    @RequestMapping("/say")
+    public Object sayHello(){
+        System.out.println("Hello World");
+        return new Person("wuxinjie");
+    }
+
+    class Person{
+
+        public Person(String name){
+            this.name = name;
+        }
+
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
