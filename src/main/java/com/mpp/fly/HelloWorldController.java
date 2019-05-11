0dbaa1a565941bee3cd137a295331747ad3988fb
Cultivators <wuxinjie13450@gmail.com>
2019-05-11 22:35:49 +0800

    将日志更改为lombok

--- src/main/java/com/mpp/fly/HelloWorldController.java
+++ src/main/java/com/mpp/fly/HelloWorldController.java
@@ -1,19 +1,17 @@
 package com.mpp.fly;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
-
     @RequestMapping("/say")
     public Object sayHello(){
-        logger.info("Hello World");
+        log.info("Hello World");
         return new Person("wuxinjie");
     }
 
